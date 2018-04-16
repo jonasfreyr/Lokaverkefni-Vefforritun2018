95854ebb77bb46b8ec27a8ed4198e5f9c35ae17e
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2018-04-16 12:30:02 +0000

    breytti database

--- Lokaverkefni/Data/database.sql
+++ Lokaverkefni/Data/database.sql
@@ -1,3 +1,10 @@
 create database 2801002260_Lokaverkefni;
 
 use 2801002260_Lokaverkefni;
+
+create Table Part(
+	PART_NUM int unique,
+    PART_DESC varchar(200),
+    PART_WEIGHT int,
+    PART_SIZE varchar(40)
+);
