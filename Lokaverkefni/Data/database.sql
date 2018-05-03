a069bfcfb1efea7930dd2f6d366ffa39109bf6b1
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2018-05-03 13:34:26 +0000

    Geri stórbreytingar við database
    Við breyttum hugmyndini okkar hvernig síðan okkar á að vera og gjörbreyttum database-inum

--- Lokaverkefni/Data/database.sql
+++ Lokaverkefni/Data/database.sql
@@ -1,34 +1,28 @@
+drop database 2801002260_Lokaverkefni_VEF;
+
 create database 2801002260_Lokaverkefni_VEF;
 
 use 2801002260_Lokaverkefni_VEF;
 
-create Table Part(
-	PART_NUM int unique primary key,
-    PART_NAME varchar(30),
-    PART_DESC varchar(200),
-    PART_WEIGHT int,
-    PART_SIZE varchar(40),
-    PART_PRICE int,
-    PART_AVAIL bool
+create table Post(
+	POST_ID INT unique PRIMARY KEY,
+    POST_IMG VARCHAR(100),
+    POST_DESC VARCHAR(300),
+    USER_ID INT unique,
+    foreign key Post(USER_ID) references Notandi(USER_ID)
+	
 );
 
-create table Shipment(
-	SHIPMENT_NUM int unique primary key,
-    SHIPMENT_CONAME varchar(30),
-    SHIPMENT_ADDRESS varchar(30),
-    SHIPMENT_COUNTRY varchar(30),
-    SHIPMENT_ARRIVAL date,
-    PART_NUM int unique,
-    foreign key (PART_NUM) references Part(PART_NUM)
+CREATE TABLE Pcomment(
+	COMMENT_ID INT unique PRIMARY KEY,
+    COMMENT_DESC VARCHAR(100),
+    POST_ID INT unique,
+    foreign key Pcomment(POST_ID) REFERENCES Post(POST_ID)
+
 );
 
-create table Sale(
-	SALE_NUM int unique primary key,
-    SALE_DATE date,
-    PART_NUM int unique,
-    SHIPMENT_NUM int unique,
-    SALE_TOTAL int,
-    foreign key (PART_NUM) references Part(PART_NUM),
-    foreign key (SHIPMENT_NUM) references Shipment(SHIPMENT_NUM)
-    
+CREATE TABLE Notandi(
+	USER_ID INT UNIQUE PRIMARY KEY,
+    USER_NAME VARCHAR(50)
+
 );
