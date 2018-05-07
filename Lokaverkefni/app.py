f5a8654b03b7dee6a38d347f242697e634122d5e
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2018-05-07 12:40:45 +0000

    Add files via upload

--- Lokaverkefni/app.py
+++ Lokaverkefni/app.py
@@ -8,8 +8,33 @@ app.config.from_object(__name__) # load config from this file
 def home():
     return render_template('index.html')
 
+@app.route('/stjornendur')
+def stjor():
+    return  render_template('stjorn.html')
+
+@app.route('/verslanir')
+def verslanir():
+    return  render_template('vefverslanir.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/nyskraning')
+def new():
+    return render_template('nyskraning.html')
+
+@app.route('/logon', methods=['POST'])
+def logon():
+    email = request.form['email']
+    passw = request.form['password']
+
+    print(email)
+    print(passw)
+
 @app.route('/new-user')
 def newuser():
+
     conn = MySQLdb.connect("tsuts.tskoli.is", "2801002260", "mypassword", "2801002260_Lokaverkefni_VEF")
     db = conn.cursor()
 
